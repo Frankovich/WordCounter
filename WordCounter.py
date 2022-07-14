1b2af02d33a83f2daaab9c6ea2abba51bfa3e7fe
KingWags <willjf44@gmail.com>
2022-07-14 15:12:55 -0400

    WordCounterInit

---
+++ WordCounter.py
@@ -0,0 +1,11 @@
+import re
+
+def countInString(searchFor, searchIn):
+    return len(re.findall(searchFor, searchIn))
+
+def countInList(searchFor, searchIn):
+    count = 0
+    for entry in searchIn:
+        if entry == searchFor:
+            count+=1
+    return count
